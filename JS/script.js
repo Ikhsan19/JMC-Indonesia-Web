373dd26a8d0ec01da28922a4d3ccff57dce5eb1e
Muhammad Ikhsan <muh.ikhsan818@gmail.com>
2024-06-10 22:11:39 +0800

    refactor: static to dynamic footer year

--- JS/script.js
+++ JS/script.js
@@ -2,23 +2,23 @@ AOS.init();
 
 let mybutton = document.getElementById("btn-back-to-top");
 
-window.onscroll = function() {
-    scrollFunction();
+window.onscroll = function () {
+  scrollFunction();
 };
 
 function scrollFunction() {
-    if (
-        document.body.scrollTop > 50 ||
-        document.documentElement.scrollTop > 50
-    ) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
 mybutton.addEventListener("click", backToTop);
 
 function backToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
+
+const year = new Date().getFullYear();
+document.getElementById("year").textContent = year;
